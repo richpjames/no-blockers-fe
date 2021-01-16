c2a97f9cefa9217c03b68c38df52aca0455d75bf
richpjames <rich@Richards-MacBook-Pro.local>
2021-01-16 16:53:48 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import PostList from "./PostList.svelte";
+</script>
+
+
+<body>
+  <h1>no blockers</h1>
+  <PostList />
+</body>
---
+++ src/PostList.svelte
@@ -0,0 +1,51 @@
+<script>
+    import { onMount } from 'svelte';
+    import Post from "./Post.svelte";
+    
+    let posts = [];
+    let error = null
+    
+    onMount(async () => {
+        const parseJSON = (resp) => (resp.json ? resp.json() : resp);
+        const checkStatus = (resp) => {
+        if (resp.status >= 200 && resp.status < 300) {
+          return resp;
+        }
+        return parseJSON(resp).then((resp) => {
+          throw resp;
+        });
+      };
+    
+        try {
+            const res = await fetch("http://localhost:1337/posts", {
+              method: "GET",
+              headers: {
+                 'Content-Type': 'application/json'
+              },
+            }).then(checkStatus)
+          .then(parseJSON);
+            posts = res
+        } catch (e) {
+            error = e
+        }
+    });
+    </script>
+    
+    {#if error !== null}
+      {error}
+    {:else}
+      <ul>
+      {#each posts as post (post.slug)}
+        <li>
+          <Post title={post.title} content={post.content} publishedAt={post.published_at} />
+        </li>
+      {/each}
+      </ul>
+    {/if}
+    
+    <style>
+      ul{
+        list-style: none;
+        width: min(95vw, 30rem)
+      }
+    </style>
