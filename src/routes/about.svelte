d9d7f9eaa5200225332ba74786e8720be8c198b3
richpjames <rich@Richards-MacBook-Pro.local>
2021-02-01 09:41:59 +0000

    refactor: about section

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,16 +6,24 @@
 
 <section>
   <p>
-    I am a developer based in West Yorkshire and this is my website!<br />
-    I've been working as a front end developer since 2019.<br /><br />
-
+    I've been obsessed with the internet since the 90s and, since 2019, have
+    been working as a web developer.
+  </p>
+  <p>
     On this site you'll find thoughts I'm having on the or projects i'm working
     on in the <a href="/blog">blog</a>
     or visual ideas I'm playing with in <a href="/">sketches</a>.
   </p>
+  <p>
+    I am hoping that writing my ideas in a public space will allow me to
+    approach problems in a different way. I hope you enjoy seeing them.
+  </p>
 </section>
 
 <style>
+  section {
+    width: min(90%, 50em);
+  }
   section p {
     font-size: 26px;
   }
