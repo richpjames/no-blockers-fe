f9a2b5fef0bbaa7296aceebd65c6b3650ed22545
richpjames <rich@Richards-MacBook-Pro.local>
2021-02-02 08:48:07 +0000

    add photos page

---
+++ src/routes/photos/index.svelte
@@ -0,0 +1,33 @@
+<script>
+  import PageTitle from "../../components/PageTitle.svelte";
+
+  import { photos, imageUrl } from "./photos.json";
+</script>
+
+<PageTitle>photos</PageTitle>
+
+{#each photos as { path, alt, orientation, location }}
+  <figure>
+    <img
+      src={`${imageUrl}/${path}`}
+      {alt}
+      class={orientation === "portrait" ? "portrait" : "landscape"}
+    />
+    <figcaption>{location}</figcaption>
+  </figure>
+{/each}
+
+<style>
+  figure {
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .landscape {
+  }
+  .portrait {
+    width: min(90%, 500px);
+  }
+</style>
